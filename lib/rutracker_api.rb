d5c95905b5a7d3bf98156815a57cec9db60bf8b3
Dima Bignyak <bignyak@bigmir.net>
2013-11-05 23:45:26 +0200

    add search function and parsing result

--- lib/rutracker_api.rb
+++ lib/rutracker_api.rb
@@ -1,15 +1,84 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'mechanize'
 class RutrackerApi
 
-  attr_accessor :term, :result
+  attr_accessor :agent
+
+  LOGIN_PAGE = 'http://login.rutracker.org/forum/login.php'
+  SEARCH_PAGE = 'http://rutracker.org/forum/tracker.php'
+
+  ORDER_OPTIONS = { date: 1,  name: 2, downloads: 4, shows: 6, seeders: 10,
+                    leechers: 11, size: 7, last_post: 8, speed_up: 12,
+                    speed_down: 13, message_count: 5, last_seed: 9 }
+  SORT_OPTIONS = {asc: 1, desc: 2}
 
   def initialize
     @agent = Mechanize.new
-    @agent.get 'http://login.rutracker.org/forum/login.php'
-    form = @agent.page.form('login-form')
-    form.login_username = ENV["RUTRACKER_LOGIN"]
-    form.login_password = ENV["RUTRACKER_PASSWORD"]
-    @agent.submit(form, form.buttons.first)
+    @agent.user_agent_alias = 'Mac Safari'
+    login
+  end
+
+  # Advance search throw rutracker
+  #
+  # @param option [Hash] the format type, `:category`, `:term`, `:sort`, `:order_by`
+  # @return [Hash] the options keys with search result
+  def search(options = {})
+    query = prepare_query_string options
+    @agent.get query
+    parse_search
+    # if /(\+|\-)?\w{3,}(\*|\s)?/ === term && !(/&/ === term)
+    #   request = 'http://rutracker.org/forum/tracker.php?nm=' + term
+    # else
+    #   raise "Wrong search term"
+    # end
+    # options[:category].split(',').each { |cid| request << '&f[]=' << cid } if options.has_key? :category
+ end
+  end
+
+  def find_user(nick)
+    @agent.get 'http://rutracker.org/forum/profile.php?mode=viewprofile&u=' + nick
+  end
+
+  private
+    def login
+      @agent.post(LOGIN_PAGE, login_username: 'username',
+                  login_password: 'pass', login: 'Вход')
+      # @agent.post(LOGIN_PAGE, login_username: ENV["RUTRACKER_LOGIN"],
+      #             login_password: ENV["RUTRACKER_PASS"], login: 'Вход')
+    end
+
+    def prepare_query_string(options)
+      prepared = { f: options[:category], nm: options[:term],
+        s: SORT_OPTIONS[options[:sort]], o: ORDER_OPTIONS[options[:order_by]] }
+      
+      query = SEARCH_PAGE + '?'
+      if prepared[:nm]
+        query.gsub! '&', ''
+        query << "nm=%22#{prepared[:nm]}%22"
+      end
+      query << "&f=#{prepared[:f]}" if prepared[:f]
+      query << "&f=#{prepared[:o]}" if prepared[:order_by]
+      query << "&f=#{prepared[:s]}" if prepared[:sort]
+      query
+    end
+
+    def parse_search
+    @agent.page.search("//table[@class='forumline tablesorter']/tbody/tr").map do |row|
+      description = row.at("td[4]").text.strip
+      torrent_id =  row.at("td[4]//a").attributes['data-topic_id'].text.to_i
+      size = row.at("td[6]/a").text.chomp(" ↓")
+      seeders = row.at("td[7]").text.to_i
+      leechers = row.at("td[8]").text.to_i
+      downloads = row.at("td[9]").text.to_i
+      date = DateTime.strptime(row.at("td[10]/u").text, '%s')
+      author = row.css(".u-name/a").text
+      user_id = row.css(".u-name/a")[0]['href'].scan(/\d+/)[0].to_i
+
+      { description: description, torrent_id: torrent_id, size: size,
+        seeders: seeders, leechers: leechers, downloads: downloads,
+        date: date, author: author, user_id: user_id }
+    end
   end
 end
+
--- spec/rutracker_api_spec.rb
+++ spec/rutracker_api_spec.rb
@@ -1,6 +1,33 @@
 require 'rutracker_api'
 describe "Rutracker Api" do
-  it "pending" do
-    pending "pending"
+  before { @tracker = RutrackerApi.new }
+  subject { @tracker }
+
+  describe "#search" do
+    describe "with invalid params" do
+      it "raise exeption with wrong search term" do
+        expect{@tracker.search("&8")}.to raise_error("Wrong search term")
+      end
+    end
+
+    describe "with valid params" do
+
+      it "returns link with term" do
+        expect( @tracker.search("Super Man") ).to eq('http://rutracker.org/forum/tracker.php?nm=Super Man')
+      end
+      it "returns link with category" do
+        expect( @tracker.search("Super Man", {category: '2,5'}) )
+          .to eq('http://rutracker.org/forum/tracker.php?nm=Super Man&f[]=2&f[]=5')
+      end
+      it "returns link with order_by key" do
+        expect( @tracker.search("Super Man", {order_by: 'name'}) )
+          .to eq('http://rutracker.org/forum/tracker.php?nm=Super Man&o=2')
+      end
+      it "returns link with sort_by key" do
+        expect( @tracker.search("Super Man", {sort_by: 'asc'}) )
+          .to eq('http://rutracker.org/forum/tracker.php?nm=Super Man&s=1')
+      end
+
+    end
   end
 end
